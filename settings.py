789ae34d8e4a698c6172a5f08197cbf6cccd4d30
Barrrowium <42115342+Barrrowium@users.noreply.github.com>
2019-02-11 15:42:34 +0000

    commit 2

---
+++ settings.py
@@ -0,0 +1,48 @@
+class Settings():
+    """A class to store settings for alien invasion."""
+
+    def __init__(self):
+        """initialize the games static settings."""
+        #screen Settings: sets the size of the window
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+
+        #ship Settings
+
+        self.ship_limit = 3
+
+        #bullet Settings
+
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (60, 0, 0)
+        self.bullets_allowed = 3
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        #How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        #alien Settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
+        #scoreing
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
